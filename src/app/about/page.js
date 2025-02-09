b32b7666962635717e9b09fcc939474bc97f82ed
Ricards Rudups <ricardsrudups99@gmail.com>
2025-02-09 21:12:07 +0000

    finished about me page.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,6 +2,9 @@
 export default function about() {
 
     return (
-    <h1>About me</h1>
+    <h1>About me</h1>,
+    <p>Hi potential employer, you have found one of my projects, (Let's just call it a work in progress) 
+    I am on a journey to gain the needed skills to work in tech, preferably as a software QA. I am very enthusiastic and
+    I am sure that with time and the opportunity to have hands on experience, I can really develop into an asset. Please give me job :)</p>
     )
 }
